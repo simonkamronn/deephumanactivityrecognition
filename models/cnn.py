768e2ae94fc6960a898b55259c2697f2815915b5
Simon Kamronn <simon@kamronn.dk>
2015-10-30 12:14:51 +0100

    Reverted to previous method of filtering

--- models/cnn.py
+++ models/cnn.py
@@ -59,7 +59,7 @@ class CNN(Model):
                 print("Conv out shape", get_output_shape(l_prev))
         else:
             l_prev = ReshapeLayer(l_prev, (batch_size, 1, sequence_length, n_features))
-            l_prev = DimshuffleLayer(l_prev, (0, 3, 2, 1))
+            # l_prev = DimshuffleLayer(l_prev, (0, 3, 2, 1))
             for n_filter, filter_size, pool_size in zip(n_filters, filter_sizes, pool_sizes):
                 self.log += "\nAdding 2D conv layer: %d x %d" % (n_filter, filter_size)
                 l_tmp = Conv2DLayer(l_prev,
