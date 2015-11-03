8a97fb332798d76df0a20ac9dcd936ba162f88cb
Simon Kamronn <simon@kamronn.dk>
2015-11-03 17:17:17 +0100

    Fixed sorting error

--- find_best_models.py
+++ find_best_models.py
@@ -2,12 +2,12 @@ import os
 import cPickle as pkl
 import numpy as np
 
-best_validation_accuracies = 0
+
 best_model = None
 
-best_validation_accuracies = [0]*10
-best_accuracy_indexes = [0]*10
-best_model_dirs = [0]*10
+best_validation_accuracies = np.empty(10, dtype=float)
+best_accuracy_indexes = np.empty(10, dtype=int)
+best_model_dirs = np.empty(10, dtype=object)
 model_directory = 'output'
 sub_dirs = sorted(os.listdir(model_directory))
 for sub_dir in sub_dirs:
@@ -17,22 +17,26 @@ for sub_dir in sub_dirs:
         best_accuracy = np.max(validations[:, 1])
         if best_accuracy > np.min(best_validation_accuracies):
             best_validation_accuracies[0] = best_accuracy
-            best_validation_accuracies.sort()
-
             best_accuracy_indexes[0] = np.argmax(validations[:, 1])
-            best_accuracy_indexes.sort()
-
             best_model_dirs[0] = sub_dir
-            best_model_dirs.sort()
+
+            # Sort according to accuracy
+            sort_idx = np.argsort(best_validation_accuracies)
+            best_validation_accuracies = np.array(best_validation_accuracies)[sort_idx]
+            best_accuracy_indexes = np.array(best_accuracy_indexes)[sort_idx]
+            best_model_dirs = np.array(best_model_dirs)[sort_idx]
 
 
-for best_model_dir, best_validation_accuracy in zip(best_model_dirs, best_validation_accuracies):
+for best_model_dir, best_validation_accuracy, best_accuracy_index \
+        in zip(best_model_dirs, best_validation_accuracies, best_accuracy_indexes):
+
     if best_validation_accuracy > 0:
         for file in os.listdir('./' + model_directory + '/' + best_model_dir):
             if file.endswith('log'):
                 best_model_log = os.path.join(model_directory, best_model_dir, file)
 
-        print("Best accuracy: %04f\nBest model: %s" % (best_validation_accuracy, best_model_dir))
+        print("Best accuracy: %04f in epoch %d\nBest model: %s" %
+              (best_validation_accuracy, best_accuracy_index, best_model_dir))
 
         log = ""
         with open(best_model_log, 'r') as f:
