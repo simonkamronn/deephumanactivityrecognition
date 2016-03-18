a040f58d704808db9efedbe167373dcec3c9878d
Simon Kamronn <simon@kamronn.dk>
2016-03-18 10:23:25 +0100

    Added min and default values to ConstrainLayer

--- lasagne_extensions/layers/constrainlayer.py
+++ lasagne_extensions/layers/constrainlayer.py
@@ -3,13 +3,14 @@ import theano.tensor as T
 
 
 class ConstrainLayer(lasagne.layers.MergeLayer):
-    def __init__(self, incoming, scale, max_value, **kwargs):
-        super(ConstrainLayer, self).__init__([incoming, scale, max_value], **kwargs)
+    def __init__(self, incoming, scale=1, min=-1e2, max=1e2, **kwargs):
+        super(ConstrainLayer, self).__init__([incoming, scale, min, max], **kwargs)
         self.scale = scale
-        self.max_value = max_value
+        self.min = min
+        self.max = max
 
     def get_output_shape_for(self, input_shapes):
         return input_shapes[0]
 
     def get_output_for(self, input, **kwargs):
-        return T.clip(input*self.scale, 0, self.max_value)
+        return T.clip(input * self.scale, self.min, self.max)
